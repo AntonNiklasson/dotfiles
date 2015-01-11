9ba50e10acbe46f9f0e9f73754389181c282cac8
Anton Niklasson <niklasson.anton@gmail.com>
2015-01-11 19:25:36 +0100

    Small additions to the install script.

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
 #! /bin/sh
 
-echo "Creating .files..."
-
 for f in ~/.dotfiles/link/.*; do
 	if test -f "$f"
 	then
-		echo "Creating ~/$(basename $f)"
 		ln -fs "$f" "$(basename $f)"
 	fi
 done
@@ -13,13 +10,15 @@ done
 # .bashrc to .bash_profile on OSX.
 if [[ $OSTYPE == darwin* ]]; then
 	ln -fs ~/.bashrc ~/.bash_profile
-	. ~/.bash_profile
+	source ~/.bash_profile
 else
-	. ~/.bashrc
+	source ~/.bashrc
 fi
 
-# Set global .gitignore
+git config --global user.name "Anton Niklasson"
+git config --global user.email "niklasson.anton@gmail.com"
 git config --global core.excludesfile '~/.gitignore'
+git config --global core.editor 'vim'
 
 
-echo "Dotfiles installed successfully!"
+echo "Dotfiles installed."
