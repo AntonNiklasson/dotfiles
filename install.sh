fe7f91b135afc36e58a33e37d58f21bc7e792ebd
Anton Niklasson <niklasson.anton@gmail.com>
2016-08-18 17:27:15 +0200

    Moved things around. Removed some old things.

---
+++ install.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+# Install some apps.
+#printf "\n\n"
+#read -p "Install Homebrew and all the apps? " -n 1 -r
+#if [[ $REPLY =~ ^[Yy]$ ]]
+#then
+	# Install Homebrew.
+	#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+	# Install Homebrew Cask.
+	#brew tap caskroom/cask
+
+	# Install all the applications.
+	#brew cask install google-chrome firefox lastpass sequel-pro slack spotify
+	#brew cask install sublime-text skim alfred divvy dropbox flux
+	#brew cask install omnidisksweeper seil skype transmission vlc
+#fi
+
+# Link up files.
+ln -is ~/.dotfiles/links/zshrc ~/.zshrc
+ln -is ~/.dotfiles/links/vimrc ~/.vimrc
+ln -is ~/.dotfiles/links/gitignore ~/.gitignore
+ln -is ~/.dotfiles/links/gitconfig ~/.gitconfig
+
+# Configure Vim with Vundle.
+rm -rf ~/.vim ~/.dotfiles/vim/bundle
+ln -is ~/.dotfiles/vim ~/.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim -c PluginInstall -c quitall
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+source ~/.zshrc
