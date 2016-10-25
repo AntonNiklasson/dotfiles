d9e24f790349a82e25c3d08e4329e7dbf26caa3c
Anton Niklasson <niklasson.anton@gmail.com>
2016-10-25 18:05:26 +0200

    Installs from brew.

--- install.sh
+++ install.sh
@@ -8,18 +8,17 @@ then
 	Install Homebrew.
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-        # Install things.
-        brew install npm tree
+        # Install things from brew.
+        brew install node npm tree composer
 
 	# Install Homebrew Cask.
 	brew tap caskroom/cask
 
-	# Install all the applications.
+        # Install all GUI applications.
         brew cask install google-chrome firefox lastpass sequel-pro slack spotify skim alfred divvy dropbox flux omnidisksweeper seil skype transmission vlc iterm2 atom fantastical hipchat appcleaner divvy macvim
 	
 	# Install some fonts.
 	brew tap caskroom/fonts
-
 	brew cask install font-inconsolata font-source-code-pro
 fi
 
