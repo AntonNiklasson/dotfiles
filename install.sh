33b9e96269f3edbd6e7d5a02fbd7ab942ee9cd31
Anton Niklasson <niklasson.anton@gmail.com>
2016-12-08 11:27:16 +0100

    Installing neovim.

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ then
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 	# Install things from brew.
-	brew install node npm tree composer
+        brew install node npm tree composer neovim/neovim/neovim
 
 	# Install Homebrew Cask.
 	brew tap caskroom/cask
