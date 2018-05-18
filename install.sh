7219fda0986dd9d7e2a8d495d875503eec8ab516
Anton Niklasson <niklasson.anton@gmail.com>
2018-05-18 21:41:15 +0200

    cleanup

--- install.sh
+++ install.sh
@@ -1,26 +1,14 @@
 #! /bin/bash
 
-# Install some apps.
-printf "\n\n"
-read -p "Install Homebrew and all the apps? " -n 1 -r
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-	Install Homebrew.
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Install Homebrew.
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-	# Install packages from brew.
-  brew install node npm tree neovim/neovim/neovim
-
-	# Configure Homebrew Cask.
-	brew tap caskroom/cask
-
-	# Install GUI applications.
-	brew cask install google-chrome firefox slack spotify skim alfred spectacle dropbox flux seil skype vlc iterm2 atom appcleaner macvim
-
-	# Install fonts.
-	brew tap caskroom/fonts
-	brew cask install font-inconsolata font-source-code-pro
-fi
+# Get some stuff from Homebrew.
+brew install node npm tree neovim/neovim/neovim
+brew tap caskroom/cask
+brew cask install google-chrome firefox slack spotify skim alfred spectacle dropbox flux seil skype vlc iterm2 atom appcleaner macvim
+brew tap caskroom/fonts
+brew cask install font-inconsolata font-source-code-pro
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -30,11 +18,5 @@ ln -is ~/.dotfiles/links/zshrc ~/.zshrc
 ln -is ~/.dotfiles/links/vimrc ~/.vimrc
 ln -is ~/.dotfiles/links/gitignore ~/.gitignore
 ln -is ~/.dotfiles/links/gitconfig ~/.gitconfig
-
-# Configure Vim with Vundle.
-rm -rf ~/.vim ~/.dotfiles/vim/bundle
-ln -is ~/.dotfiles/vim ~/.vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim -c PluginInstall -c quitall
 
 source ~/.zshrc
--- vim/ftplugin/tex.vim
+++
@@ -1,6 +0,0 @@
-set wrap
-set nolist
-set textwidth=0
-set spell
-
-let g:Tex_SmartKeyQuote=0
