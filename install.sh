e7fb38dc50cc80da0bfe610810e0bff95db28f95
Anton Niklasson <niklasson.anton@gmail.com>
2018-09-03 15:45:34 +0200

    changes

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Get some stuff from Homebrew.
-brew install node npm tree neovim/neovim/neovim
+brew install node npm tree neovim/neovim/neovim coreutils
 brew tap caskroom/cask
 brew cask install google-chrome firefox slack spotify skim alfred spectacle dropbox flux seil skype vlc iterm2 atom appcleaner macvim
 brew tap caskroom/fonts
