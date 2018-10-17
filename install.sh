227243d03df0942a40c8bcdbd4d20e0a6d4f9b2c
Anton Niklasson <niklasson.anton@gmail.com>
2018-10-17 14:08:00 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ git clone git@github.com:AntonNiklasson/dotfiles.git .dotfiles
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Get some stuff from Homebrew.
-brew install node npm tree neovim/neovim/neovim coreutils
+brew install node npm tree coreutils hub
 brew tap caskroom/cask
 brew cask install google-chrome firefox slack spotify skim alfred spectacle dropbox flux seil skype vlc iterm2 atom appcleaner macvim
 brew tap caskroom/fonts
