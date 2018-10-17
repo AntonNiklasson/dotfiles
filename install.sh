76b59fac43114be13b96d65db726e2d162536e79
Anton Niklasson <niklasson.anton@gmail.com>
2018-10-17 14:10:36 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -6,11 +6,13 @@ git clone git@github.com:AntonNiklasson/dotfiles.git .dotfiles
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Get some stuff from Homebrew.
-brew install node npm tree coreutils hub
+brew install node npm tree neovim/neovim/neovim coreutils diff-so-fancy hub
 brew tap caskroom/cask
 brew cask install google-chrome firefox slack spotify skim alfred spectacle dropbox flux seil skype vlc iterm2 atom appcleaner macvim
 brew tap caskroom/fonts
 brew cask install font-inconsolata font-source-code-pro
+
+npm i -g yarn spaceship-prompt
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
