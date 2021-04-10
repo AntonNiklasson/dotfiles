7a8d5329561bbe952bac2927ff97899eb89bc4a9
Anton Niklasson <anton@finhack.se>
2021-04-10 13:40:40 +0200

    updates

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Install Homebrew.
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Add taps
 brew tap homebrew/cask
