06c1e6da048baf58dee4f9811922856e44007e2f
Anton Niklasson <anton@finhack.se>
2021-04-27 09:19:47 +0200

    updates

--- install-steps/macos.sh
+++ install-steps/macos.sh
@@ -11,3 +11,16 @@ defaults write com.apple.dock mru-spaces -int 0
 
 # Don't store screenshots in ~/Desktop
 defaults write com.apple.screencapture location ~/Dropbox/Screenshots
+
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+
+# Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+# Trackpad: enable three finger drag
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
+
+# Enable full keyboard access for all controls
+# (e.g. enable Tab in modal dialogs)
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
