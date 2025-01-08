42bfd27fd243f5596c3a8f5783d8af2e1b6351aa
Anton Niklasson <niklasson.anton@gmail.com>
2025-01-08 09:51:14 +0100

    disable markdown lsp

--- links/.config/nvim/lua/config/lazy.lua
+++ links/.config/nvim/lua/config/lazy.lua
@@ -12,7 +12,6 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.coding.yanky" },
 		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
-		{ import = "lazyvim.plugins.extras.lang.markdown" },
 		{ import = "lazyvim.plugins.extras.lang.tailwind" },
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
 		{ import = "lazyvim.plugins.extras.editor.refactoring" },
