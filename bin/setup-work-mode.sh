48fa0eb9b861b8f265db2f4f2df684100faf30c8
Anton Niklasson <niklasson.anton@gmail.com>
2025-01-27 20:34:22 +0100

    play with some kind of "init work mode" chain

---
+++ bin/setup-work-mode.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Setup for work
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 💻
+
+# Set system volume to 50%
+open "raycast://extensions/raycast/system/set-volume-to-50"
+
+# Launch the coding setup
+# open "raycast://customWindowManagementCommand?&name=Coding%20"
+
+# Connect AirPods
+open "raycast://extensions/VladCuciureanu/toothpick/connect-favorite-device-1"
+
+# Dismiss all notifications
+open "raycast://script-commands/d1d5f97ad62a6692d70fc17da0a71157"
