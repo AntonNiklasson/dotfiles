28fcda359b96963f8b823e4e03505d98045f8ff9
Anton Niklasson <niklasson.anton@gmail.com>
2026-02-05 08:57:31 +0100

    configure neomvim snacks

---
+++ links/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+	"folke/snacks.nvim",
+	opts = {
+		picker = {
+			sources = {
+				files = { hidden = true },
+				smart = { hidden = true },
+				grep = { hidden = true },
+				grep_word = { hidden = true },
+			},
+		},
+	},
+}
