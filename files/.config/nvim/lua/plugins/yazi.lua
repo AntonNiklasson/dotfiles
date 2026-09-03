3eab79ebbdd94f9e53e9343ac81ec64f9befaa28
Anton Niklasson <niklasson.anton@gmail.com>
2026-09-03 09:23:01 +0200

    nvim: point yazi.nvim grep at fzf-lua
    Defaults target telescope and grug-far, neither of which is installed,
    so <c-s> grep and <c-g> replace both errored. Wire grep to fzf-lua and
    disable the replace keymap. Adds <leader>ec and <leader>er.

--- files/.config/nvim/lua/plugins/yazi.lua
+++ files/.config/nvim/lua/plugins/yazi.lua
@@ -10,11 +10,29 @@ return {
 			"<cmd>Yazi<cr>",
 			desc = "Explore filesystem",
 		},
+		{
+			"<leader>ec",
+			"<cmd>Yazi cwd<cr>",
+			desc = "Explore cwd",
+		},
+		{
+			"<leader>er",
+			"<cmd>Yazi toggle<cr>",
+			desc = "Resume last yazi session",
+		},
 	},
 	opts = {
 		open_for_directories = true,
 		keymaps = {
 			show_help = "?",
+			-- default replacer is grug-far.nvim, which isn't installed
+			replace_in_directory = false,
+		},
+		integrations = {
+			-- default is telescope, which isn't installed
+			grep_in_directory = "fzf-lua",
+			grep_in_selected_files = "fzf-lua",
+			picker_add_copy_relative_path_action = "snacks.picker",
 		},
 	},
 }
